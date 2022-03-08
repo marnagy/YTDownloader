fdce50107fdf631808d366cca7c1243de7f54fdc
Marek Nagy <62247071@cuni.cz>
2022-03-08 13:12:49 +0100

    Added extension methods for more readable code

---
+++ YTDownloader/YouTubeVideoExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VideoLibrary;
+
+namespace YTDownloader
+{
+	public static class YouTubeVideoExtensions
+	{
+		public static bool IsAudioOnly(this YouTubeVideo video)
+			=> video.Format == VideoFormat.Unknown &&
+				video.AudioFormat != AudioFormat.Unknown;
+		public static bool IsVideoOnly(this YouTubeVideo video)
+			=> video.Format == VideoFormat.Mp4
+				&& video.AudioFormat == AudioFormat.Unknown;
+	}
+}
